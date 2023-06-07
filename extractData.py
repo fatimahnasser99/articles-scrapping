aca863c794e0d1801175d54aa36098dd0d2f9d8d
Fatima Nasser <fatimahnasser1999@gmail.com>
2023-06-07 09:11:28 +0300

    extract articles

--- extractData.py
+++ extractData.py
@@ -24,7 +24,7 @@ def get_topics(url):
     return topics_list
 
 
-def get_articles(topics_list):
+def get_articles_list(topics_list):
     keys = ['name', 'url', 'topic' ]
     articles_list = []
     for topic in topics_list:
@@ -49,3 +49,20 @@ def get_articles(topics_list):
     return articles_list
     
 
+def get_articles(articles_frame):
+  articles_data = []
+  for index, row in articles_frame.iterrows():
+      article_data = scrapping.scrapping(row['url'])
+      #remove unecessary data
+      target_text = f'Best {row.name} Books'
+      target_element = article_data.find('h2', string=target_text)
+      if target_element:
+          next_siblings = target_element.find_next_siblings()
+          for sibling in next_siblings:
+              sibling.extract()
+          target_element.extract()
+      #get article as text    
+      row['article'] = article_data.get_text()
+      articles_data.append(row)
+
+  return articles_data
